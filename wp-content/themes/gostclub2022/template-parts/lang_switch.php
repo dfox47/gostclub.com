7a369a230001b337be4e38a35587195d648fe560
dfox <dfox@foxartbox.com>
2022-09-10 16:57:08 +0300

    Lang switcher to separate file

--- wp-content/themes/gostclub2022/template-parts/lang_switch.php
+++ wp-content/themes/gostclub2022/template-parts/lang_switch.php
@@ -1,18 +1,18 @@
 
-<div class="header_lang_switcher">
-	<div class="header_lang_switcher__current" data-x="<?php echo get_bloginfo("language"); ?>"><?php echo substr(get_bloginfo("language"), 0, 2); ?></div>
+<div class="lang_switcher">
+	<div class="lang_switcher__current" data-x="<?php echo get_bloginfo("language"); ?>"><?php echo substr(get_bloginfo("language"), 0, 2); ?></div>
 
-	<ul class="header_lang_switcher_list">
+	<ul class="lang_switcher_list">
 		<?php if (get_bloginfo("language") !== 'en-US') { ?>
-			<li class="header_lang_switcher_list__item"><a class="header_lang_switcher_list__link" href="/en/">en</a></li>
+			<li class="lang_switcher_list__item"><a class="lang_switcher_list__link" href="/en/">en</a></li>
 		<?php } ?>
 
 		<?php if (get_bloginfo("language") !== 'ru-RU') { ?>
-			<li class="header_lang_switcher_list__item"><a class="header_lang_switcher_list__link" href="/ru/">ru</a></li>
+			<li class="lang_switcher_list__item"><a class="lang_switcher_list__link" href="/ru/">ru</a></li>
 		<?php } ?>
 
 		<?php if (get_bloginfo("language") !== 'bg-BG') { ?>
-			<li class="header_lang_switcher_list__item"><a class="header_lang_switcher_list__link" href="/">bg</a></li>
+			<li class="lang_switcher_list__item"><a class="lang_switcher_list__link" href="/">bg</a></li>
 		<?php } ?>
 	</ul>
 </div>
