f6d3016d1e00013a8adb3f2c6c89a0adfc89b64b
dfox <dfox@foxartbox.com>
2022-09-12 17:44:03 +0300

    category.php refactoring

--- wp-content/themes/gostclub2022/category.php
+++ wp-content/themes/gostclub2022/category.php
@@ -1,43 +1,50 @@
 
 <?php get_header(); ?>
 
-<?php // thumb img url
-$thumbUrl = '';
+<main data-tmp="category.php">
+	<div class="wrap">
+		<div class="content">
+			<div class="content_main">
+				<?php get_breadcrumbs(); ?>
 
-if (get_the_post_thumbnail_url()) {
-	$thumbUrl = str_replace('https://' . $_SERVER['SERVER_NAME'], '', get_the_post_thumbnail_url());
-} ?>
+				<h1><?php single_cat_title(); ?></h1>
 
-<main class="main_content_wrap" data-tmp="category.php">
-	<div class="main_content">
-		<div class="wrap4">
-			<h1><?php single_cat_title(); ?></h1>
+				<?php // posts
+				if (have_posts()) { ?>
+					<ul class="cat_items_list js-news-list">
+						<?php // The Loop
+						while (have_posts()) : the_post(); ?>
+							<?php // thumb img url
+							$thumbUrl = '';
 
-			<?php // posts
-			if (have_posts()) { ?>
-				<ul class="news_list js-news-list">
-					<?php // The Loop
-					while (have_posts()) : the_post(); ?>
-						<li class="news_list__item">
-							<a class="news_list__link" href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title(); ?>" style="background-image: url(<?php echo $thumbUrl; ?>)"></a>
-						</li>
-					<?php endwhile; ?>
-				</ul>
+							if (get_the_post_thumbnail_url()) {
+								$thumbUrl = str_replace('https://' . $_SERVER['SERVER_NAME'], '', get_the_post_thumbnail_url());
+							} ?>
 
-				<?php // Загрузить ещё
-				global $wp_query;
+							<li class="cat_items_list__item">
+								<a class="cat_items_list__link" href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title(); ?>" style="background-image: url(<?php echo $thumbUrl; ?>)"></a>
+							</li>
+						<?php endwhile; ?>
+					</ul>
 
-				$total_pages    = $wp_query -> max_num_pages;
-				$paged          = (get_query_var('paged')) ? get_query_var('paged') : 1;
+					<?php // Загрузить ещё
+					global $wp_query;
 
-				if ($paged !== $total_pages) { ?>
-					<div class="pagination_wrap js-pagination-more">
-						<?php next_posts_link( 'Загрузить ещё' ); ?>
-					</div>
+					$total_pages    = $wp_query -> max_num_pages;
+					$paged          = (get_query_var('paged')) ? get_query_var('paged') : 1;
+
+					if ($paged !== $total_pages) { ?>
+						<div class="pagination_wrap js-pagination-more">
+							<?php next_posts_link( 'Загрузить ещё' ); ?>
+						</div>
+					<?php } ?>
+
+					<div class="pagination_result js-pagination-result"></div>
 				<?php } ?>
+			</div>
 
-				<div class="pagination_result js-pagination-result"></div>
-			<?php } ?>
+			<?php // aside
+			include "template-parts/aside.php"; ?>
 		</div>
 	</div>
 </main>
