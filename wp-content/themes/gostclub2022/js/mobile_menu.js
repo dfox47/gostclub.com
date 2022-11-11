12e414e2b656f6c41c42185327f6bae640f36d6c
dfox <dfox@foxartbox.com>
2022-11-11 16:16:53 +0200

    Mobile menu JS refactoring

--- wp-content/themes/gostclub2022/js/mobile_menu.js
+++ wp-content/themes/gostclub2022/js/mobile_menu.js
@@ -1,6 +1,6 @@
 // mobile menu toggle
-document.querySelectorAll('.js-mobile-menu-toggle').forEach((btn) => {
-	btn.addEventListener('click', () => {
+document.querySelectorAll('.js-mobile-menu-toggle').forEach((e) => {
+	e.addEventListener('click', () => {
 		document.documentElement.classList.toggle('mobile_menu_active')
 	})
 })
