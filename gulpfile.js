98bac2bb4331e14745bd7e23cf6dccbc9659771c
dfox <dfox@foxartbox.com>
2022-12-09 15:14:16 +0200

    gulp update

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,18 @@
-
 // npm install -g gulp-cli
 // npm install gulp gulp-csso gulp-concat vinyl-ftp gulp-util gulp-rename gulp-sass gulp-uglify --save-dev
 
 'use strict'
 
-let fs              = require('fs')
-let config          = JSON.parse(fs.readFileSync('../config.json'))
-let ftp             = require('vinyl-ftp')
-let gulp            = require('gulp')
-let gutil           = require('gulp-util')
-let concat          = require('gulp-concat')
-let cssMinify       = require('gulp-csso')
-let rename          = require('gulp-rename')
-let sass            = require('gulp-sass')(require('sass'))
-let uglify          = require('gulp-uglify')
+const fs            = require('fs')
+const config        = JSON.parse(fs.readFileSync('../config.json'))
+const ftp           = require('vinyl-ftp')
+const gulp          = require('gulp')
+const gutil         = require('gulp-util')
+const concat        = require('gulp-concat')
+const cssMinify     = require('gulp-csso')
+const rename        = require('gulp-rename')
+const sass          = require('gulp-sass')(require('sass'))
+const uglify        = require('gulp-uglify')
 
 // FTP config
 const host          = config.host
@@ -21,34 +20,32 @@ const password      = config.password
 const port          = config.port
 const user          = config.user
 
-let remoteFolder    = '/gostclub.com/public_html/wp-content/themes/supermag/'
-// let remoteFolder    = '/gostclub.com/public_html/wp-content/themes/gostclub2022/'
-let remoteHooks     = remoteFolder + 'acmethemes/hooks/'
-let remoteAssets    = remoteFolder + 'assets/'
-let remoteCss       = remoteAssets + 'css/'
-let remoteJs        = remoteAssets + 'js/'
-let remoteParts     = remoteFolder + 'template-parts/'
+const remoteFolder      = '/gostclub.com/public_html/wp-content/themes/supermag/'
+const remoteHooks       = remoteFolder + 'acmethemes/hooks/'
+const remoteAssets      = remoteFolder + 'assets/'
+const remoteCss         = remoteAssets + 'css/'
+const remoteJs          = remoteAssets + 'js/'
+const remoteParts       = remoteFolder + 'template-parts/'
 
-let localFolder     = 'wp-content/themes/supermag/'
-// let localFolder     = 'wp-content/themes/gostclub2022/'
-let localHooks      = localFolder + 'acmethemes/hooks/'
-let localAssets     = localFolder + 'assets/'
-let localCss        = localAssets + 'css/'
-let localJs         = localAssets + 'js/'
-let localParts      = localFolder + 'template-parts/'
+const localFolder       = 'wp-content/themes/supermag/'
+const localHooks        = localFolder + 'acmethemes/hooks/'
+const localAssets       = localFolder + 'assets/'
+const localCss          = localAssets + 'css/'
+const localJs           = localAssets + 'js/'
+const localParts        = localFolder + 'template-parts/'
 
 
 
 // GOST club template
-let gostClubRemote          = '/gostclub.com/public_html/wp-content/themes/gostclub2022/'
-let gostClubRemoteCss       = gostClubRemote + 'css/'
-let gostClubRemoteJs        = gostClubRemote + 'js/'
-let gostClubRemoteParts     = gostClubRemote + 'template-parts/'
+const gostClubRemote        = '/gostclub.com/public_html/wp-content/themes/gostclub2022/'
+const gostClubRemoteCss     = gostClubRemote + 'css/'
+const gostClubRemoteJs      = gostClubRemote + 'js/'
+const gostClubRemoteParts   = gostClubRemote + 'template-parts/'
 
-let gostClubLocal           = 'wp-content/themes/gostclub2022/'
-let gostClubLocalCss        = gostClubLocal + 'css/'
-let gostClubLocalJs         = gostClubLocal + 'js/'
-let gostClubLocalParts      = gostClubLocal + 'template-parts/'
+const gostClubLocal         = 'wp-content/themes/gostclub2022/'
+const gostClubLocalCss      = gostClubLocal + 'css/'
+const gostClubLocalJs       = gostClubLocal + 'js/'
+const gostClubLocalParts    = gostClubLocal + 'template-parts/'
 
 
 
@@ -64,7 +61,7 @@ function getFtpConnection() {
 	});
 }
 
-let conn = getFtpConnection()
+const conn = getFtpConnection()
 
 
 
@@ -117,10 +114,10 @@ gulp.task('gostTemplateParts', function () {
 
 
 gulp.task('watch', function() {
-	gulp.watch(gostClubLocal + '*.php',     gulp.series('gostPhp'))
-	gulp.watch(gostClubLocalCss + '**/*',   gulp.series('gostCss', 'gostCssCopy'))
-	gulp.watch(gostClubLocalJs + '**/*',    gulp.series('gostJs', 'gostJsCopy'))
-	gulp.watch(gostClubLocalParts + '**/*', gulp.series('gostTemplateParts'))
+	gulp.watch(gostClubLocal + '*.php',         gulp.series('gostPhp'))
+	gulp.watch(gostClubLocalCss + '**/*',       gulp.series('gostCss', 'gostCssCopy'))
+	gulp.watch(gostClubLocalJs + '**/*',        gulp.series('gostJs', 'gostJsCopy'))
+	gulp.watch(gostClubLocalParts + '**/*',     gulp.series('gostTemplateParts'))
 })
 
 gulp.task('default', gulp.series('watch'))
