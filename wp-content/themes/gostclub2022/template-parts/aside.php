c5f9b947f9b046b4f90b72a4737449c11a8adc50
dfox <dfox@foxartbox.com>
2022-12-10 12:15:24 +0200

    Aside titles for images

--- wp-content/themes/gostclub2022/template-parts/aside.php
+++ wp-content/themes/gostclub2022/template-parts/aside.php
@@ -1,6 +1,19 @@
 <?php $i        = '/wp-content/themes/gostclub2022/i';
 $journalImg     = $i . '/journal';
-$dummImg        = $i . '/dumm.png'; ?>
+$dummImg        = $i . '/dumm.png';
+
+$currentLang = substr(get_bloginfo("language"), 0, 2);
+
+$txtGostJournal = "ГОСТ журнал";
+
+if ($currentLang == 'bg') {
+	$txtGostJournal = "ГОСТ списание";
+}
+elseif ($currentLang == 'en') {
+	$txtGostJournal = "GOST journal";
+}
+
+?>
 
 <div class="aside">
 	<div class="mobile_hide">
@@ -53,51 +66,69 @@ $dummImg        = $i . '/dumm.png'; ?>
 
 	<ul class="aside_list">
 		<li class="aside_list__item">
-			<a class="aside_list__link" href="//issuu.com/zenitafish/docs/_____11" target="_blank" title="ГОСТ журнал #10">
-				<img class="js-img-scroll" src="<?php echo $dummImg; ?>" data-src="<?php echo $journalImg; ?>/1.jpg" alt="ГОСТ журнал #10" />
+			<a class="aside_list__link" href="//issuu.com/zenitafish/docs/_____11" target="_blank" title="<?= $txtGostJournal ?> #11">
+				<img class="js-img-scroll" src="<?php echo $dummImg; ?>" data-src="<?php echo $journalImg; ?>/1.jpg" alt="<?= $txtGostJournal ?> #11">
+			</a>
+		</li>
+ 
+		<li class="aside_list__item">
+			<a class="aside_list__link" href="//issuu.com/zenitafish/docs/small" target="_blank" title="<?= $txtGostJournal ?> #10">
+				<img class="js-img-scroll" src="<?php echo $dummImg; ?>" data-src="<?php echo $journalImg; ?>/2.jpg" alt="<?= $txtGostJournal ?> #10">
 			</a>
 		</li>
 
 		<li class="aside_list__item">
-			<a class="aside_list__link" href="//issuu.com/zenitafish/docs/small" target="_blank" title="ГОСТ журнал #10">
-				<img class="js-img-scroll" src="<?php echo $dummImg; ?>" data-src="<?php echo $journalImg; ?>/2.jpg" alt="ГОСТ журнал #10" />
+			<a class="aside_list__link" href="//issuu.com/zenitafish/docs/web-isuu" target="_blank" title="<?= $txtGostJournal ?> #9">
+				<img class="js-img-scroll" src="<?php echo $dummImg; ?>" data-src="<?php echo $journalImg; ?>/3.jpg" alt="<?= $txtGostJournal ?> #9">
 			</a>
 		</li>
 
 		<li class="aside_list__item">
-			<a class="aside_list__link" href="//issuu.com/zenitafish/docs/web-isuu" target="_blank"><img class="js-img-scroll" src="<?php echo $dummImg; ?>" data-src="<?php echo $journalImg; ?>/3.jpg" alt="" /></a>
+			<a class="aside_list__link" href="//issuu.com/zenitafish/docs/____8" target="_blank" title="<?= $txtGostJournal ?> #8">
+				<img class="js-img-scroll" src="<?php echo $dummImg; ?>" data-src="<?php echo $journalImg; ?>/4.jpg" alt="<?= $txtGostJournal ?> #8">
+			</a>
 		</li>
 
 		<li class="aside_list__item">
-			<a class="aside_list__link" href="//issuu.com/zenitafish/docs/____8" target="_blank"><img class="js-img-scroll" src="<?php echo $dummImg; ?>" data-src="<?php echo $journalImg; ?>/4.jpg" alt="" /></a>
+			<a class="aside_list__link" href="//issuu.com/zenitafish/docs/spisanie_br_7_fina" target="_blank" title="<?= $txtGostJournal ?> #7">
+				<img class="js-img-scroll" src="<?php echo $dummImg; ?>" data-src="<?php echo $journalImg; ?>/5.jpg" alt="<?= $txtGostJournal ?> #7">
+			</a>
 		</li>
 
 		<li class="aside_list__item">
-			<a class="aside_list__link" href="//issuu.com/zenitafish/docs/spisanie_br_7_fina" target="_blank"><img class="js-img-scroll" src="<?php echo $dummImg; ?>" data-src="<?php echo $journalImg; ?>/5.jpg" alt="" /></a>
+			<a class="aside_list__link" href="//issuu.com/zenitafish/docs/magazine_01" target="_blank" title="<?= $txtGostJournal ?> #6">
+				<img class="js-img-scroll" src="<?php echo $dummImg; ?>" data-src="<?php echo $journalImg; ?>/6.jpg" alt="<?= $txtGostJournal ?> #6">
+			</a>
 		</li>
 
 		<li class="aside_list__item">
-			<a class="aside_list__link" href="//issuu.com/zenitafish/docs/magazine_01" target="_blank"><img class="js-img-scroll" src="<?php echo $dummImg; ?>" data-src="<?php echo $journalImg; ?>/6.jpg" alt="" /></a>
+			<a class="aside_list__link" href="//issuu.com/zenitafish" target="_blank" title="<?= $txtGostJournal ?> #5">
+				<img class="js-img-scroll" src="<?php echo $dummImg; ?>" data-src="<?php echo $journalImg; ?>/7.jpg" alt="<?= $txtGostJournal ?> #5">
+			</a>
 		</li>
 
 		<li class="aside_list__item">
-			<a class="aside_list__link" href="//issuu.com/zenitafish" target="_blank"><img class="js-img-scroll" src="<?php echo $dummImg; ?>" data-src="<?php echo $journalImg; ?>/7.jpg" alt="" /></a>
+			<a class="aside_list__link" href="//issuu.com/zenitafish" target="_blank" title="<?= $txtGostJournal ?> #4">
+				<img class="js-img-scroll" src="<?php echo $dummImg; ?>" data-src="<?php echo $journalImg; ?>/8.jpg" alt="<?= $txtGostJournal ?> #4">
+			</a>
 		</li>
 
 		<li class="aside_list__item">
-			<a class="aside_list__link" href="//issuu.com/zenitafish" target="_blank"><img class="js-img-scroll" src="<?php echo $dummImg; ?>" data-src="<?php echo $journalImg; ?>/8.jpg" alt="" /></a>
+			<a class="aside_list__link" href="//issuu.com/zenitafish" target="_blank" title="<?= $txtGostJournal ?> #3">
+				<img class="js-img-scroll" src="<?php echo $dummImg; ?>" data-src="<?php echo $journalImg; ?>/9.jpg" alt="<?= $txtGostJournal ?> #3">
+			</a>
 		</li>
 
 		<li class="aside_list__item">
-			<a class="aside_list__link" href="//issuu.com/zenitafish" target="_blank"><img class="js-img-scroll" src="<?php echo $dummImg; ?>" data-src="<?php echo $journalImg; ?>/9.jpg" alt="" /></a>
+			<a class="aside_list__link" href="//issuu.com/zenitafish/docs/gost_web" target="_blank" title="<?= $txtGostJournal ?> #2">
+				<img class="js-img-scroll" src="<?php echo $dummImg; ?>" data-src="<?php echo $journalImg; ?>/10.jpg" alt="<?= $txtGostJournal ?> #2">
+			</a>
 		</li>
 
 		<li class="aside_list__item">
-			<a class="aside_list__link" href="//issuu.com/zenitafish/docs/gost_web" target="_blank"><img class="js-img-scroll" src="<?php echo $dummImg; ?>" data-src="<?php echo $journalImg; ?>/10.jpg" alt="" /></a>
-		</li>
-
-		<li class="aside_list__item">
-			<a class="aside_list__link" href="//issuu.com/zenitafish/docs/spisanie_internet.compressed" target="_blank"><img class="js-img-scroll" src="<?php echo $dummImg; ?>" data-src="<?php echo $journalImg; ?>/11.jpg" alt="" /></a>
+			<a class="aside_list__link" href="//issuu.com/zenitafish/docs/spisanie_internet.compressed" target="_blank" title="<?= $txtGostJournal ?> #1">
+				<img class="js-img-scroll" src="<?php echo $dummImg; ?>" data-src="<?php echo $journalImg; ?>/11.jpg" alt="<?= $txtGostJournal ?> #1">
+			</a>
 		</li>
 	</ul>
 </div>
