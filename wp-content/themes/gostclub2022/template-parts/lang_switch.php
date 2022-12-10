67a2aad2cedf733b26af1365d4a6c8c9b745fd06
dfox <dfox@foxartbox.com>
2022-12-10 12:05:58 +0200

    Lang switch optimisation

--- wp-content/themes/gostclub2022/template-parts/lang_switch.php
+++ wp-content/themes/gostclub2022/template-parts/lang_switch.php
@@ -1,7 +1,5 @@
-<?php $currentLang = get_bloginfo("language"); ?>
-
 <div class="lang_switcher">
-	<div class="lang_switcher__current"><?php echo substr($currentLang, 0, 2); ?></div>
+	<div class="lang_switcher__current"><?php echo substr(get_bloginfo("language"), 0, 2); ?></div>
 
 	<div class="lang_switcher_list">
 		<?php if (function_exists('the_msls')) the_msls(); ?>
