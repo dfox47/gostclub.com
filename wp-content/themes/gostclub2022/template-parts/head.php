870f49ff68273628a89b625559eadd710ef05035
dfox <dfox@foxartbox.com>
2022-12-23 14:18:07 +0200

    Home slider updates

--- wp-content/themes/gostclub2022/template-parts/head.php
+++ wp-content/themes/gostclub2022/template-parts/head.php
@@ -55,7 +55,7 @@ if (is_front_page()) {
 	<div class="hero_slider">
 		<div class="js-owl-carousel-auto owl-carousel">
 			<a class="hero_slider__item js-img-bg" href="//tzarsimeon.bg/" target="_blank" data-src="<?php echo $heroImg; ?>/9.jpg"></a>
-			<div class="hero_slider__item js-img-bg" data-src="<?php echo $heroImg; ?>/8.jpg"></div>
+			<a class="hero_slider__item js-img-bg" href="//www.instagram.com/gostclub.balkans/" data-src="<?php echo $heroImg; ?>/8.jpg"></a>
 			<div class="hero_slider__item js-img-bg" data-src="<?php echo $heroImg; ?>/2.jpg"></div>
 			<div class="hero_slider__item js-img-bg" data-src="<?php echo $heroImg; ?>/5.jpg"></div>
 			<div class="hero_slider__item js-img-bg" data-src="<?php echo $heroImg; ?>/6.jpg"></div>
