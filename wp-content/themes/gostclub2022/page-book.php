c7101e3eaa0cd5800c369bbcbefcef470bf2e277
dfox <dfox@foxartbox.com>
2023-03-18 14:37:11 +0200

    Book. Nav php

--- wp-content/themes/gostclub2022/page-book.php
+++ wp-content/themes/gostclub2022/page-book.php
@@ -6,7 +6,7 @@
 <?php //header
 get_header(); ?>
 
-	<div id="book_wrap" class="book wrap2">
+	<div id="book_wrap" class="wrap2">
 		<?php // content
 		the_content();
 
@@ -16,6 +16,16 @@ get_header(); ?>
 				the_post();
 			}
 		} ?>
+
+		<ul class="book_nav">
+			<li class="book_nav__item">
+				<a class="book_nav__link book_nav__link--prev js-book-prev" href="javascript:void(0);"></a>
+			</li>
+
+			<li>
+				<a class="book_nav__link book_nav__link--next js-book-next" href="javascript:void(0);"></a>
+			</li>
+		</ul>
 	</div>
 
 <?php // footer
