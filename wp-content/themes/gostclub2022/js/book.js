c9163b2b260d777bfa9ff103c1dd6095c7ef777b
dfox <dfox@foxartbox.com>
2023-03-18 11:24:36 +0200

    JS. Book updates

--- wp-content/themes/gostclub2022/js/book.js
+++ wp-content/themes/gostclub2022/js/book.js
@@ -1,5 +1,7 @@
+// more info | https://github.com/yapro/wowbook
+
 $(document).ready(function() {
-	$('#book').wowBook({
+	$('.js-journal').wowBook({
 		centeredWhenClosed: true,
 		controls: {
 			back:           '.js-book-back',
@@ -14,58 +16,11 @@ $(document).ready(function() {
 			zoomOut:        '#zoomout'
 		},
 		hardcovers: true,
-		height: 500,
+		height: 840,
 		numberedPages: [1,-2],
-		scaleToFit: "#main",
+		scaleToFit: "#book_wrap",
 		thumbnailsPosition: 'bottom',
 		turnPageDuration: 1000,
-		width: 800
-	})
-
-	var book = $.wowBook("#features")
-
-	function rebuildThumbnails() {
-		book.destroyThumbnails()
-		book.showThumbnails()
-
-		let $thumbsHolder = $("#thumbs_holder")
-
-		$thumbsHolder.css("marginTop", - $thumbsHolder.height() / 2)
-	}
-
-	$("#thumbs_position button").on("click", function() {
-		var position = $(this).text().toLowerCase()
-
-		if ($(this).data("customized")) {
-			position = "top"
-			book.opts.thumbnailsParent = "#thumbs_holder";
-		}
-		else {
-			book.opts.thumbnailsParent = "body";
-		}
-
-		book.opts.thumbnailsPosition = position
-
-		rebuildThumbnails()
-	})
-
-	$("#thumb_automatic").click(function() {
-		book.opts.thumbnailsSprite = null
-		book.opts.thumbnailWidth = null
-		rebuildThumbnails();
-	})
-
-	$("#thumb_sprite").click(function() {
-		book.opts.thumbnailsSprite = "images/thumbs.jpg"
-		book.opts.thumbnailWidth = 136
-		rebuildThumbnails()
-	})
-
-	$("#thumbs_size button").click(function() {
-		var factor = 0.02 * ( $(this).index() ? -1 : 1 )
-
-		book.opts.thumbnailScale = book.opts.thumbnailScale + factor
-
-		rebuildThumbnails()
+		width: 1200
 	})
 })
